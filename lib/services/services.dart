438f006affdf39ac411bc0e01fe964d27ac0b28f
infinityinfospace <infinityinfo.space@gmail.com>
2023-02-10 09:06:53 +0530

    Changes

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -5,9 +5,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_flutter/model/user_model.dart';
 
 class ApiService {
+
   String userUrl = 'https://reqres.in/api/users?page=2';
 
  Future<List<UserModel>> getUsers() async {
+
     Response response = await get(Uri.parse(userUrl));
     if (response.statusCode == 200) {
       final List result = jsonDecode(response.body)['data'];
@@ -16,5 +18,7 @@ class ApiService {
       throw Exception(response.reasonPhrase);
     }
   }
+
 }
+
 final userProvider=Provider<ApiService>((ref)=>ApiService());
